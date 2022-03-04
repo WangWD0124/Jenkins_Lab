206ffaa5adf93c532fc04d8716e72adf23af1ed3
WangWD0124 <1245436962@qq.com>
2022-03-04 23:06:11 +0800

    第一次提交

---
+++ src/main/java/com/example/jenkins_springcloud_lab/JenkinsSpringCloudLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_springcloud_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsSpringCloudLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsSpringCloudLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins_springcloud_lab/JenkinsSpringCloudLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_springcloud_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsSpringCloudLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
